ec87fa69ae18e695e5e9c38fbc13646a26563d9f
Mike Machado <mike@machadolab.com>
2017-09-18 13:13:08 -0700

    Add pixel_tester firmware

---
+++ firmware/pixel_tester/pixel_tester.ino
@@ -0,0 +1,34 @@
+#include <Adafruit_NeoPixel.h>
+
+#define PIN            6
+#define NUMPIXELS      2
+#define BRIGHTNESS     30
+#define DELAY_MS       200
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  pixels.begin();
+}
+
+void loop() {
+
+  for(int i=0;i<NUMPIXELS;i++){
+
+    pixels.setPixelColor(i, pixels.Color(255,0,0));
+    pixels.setBrightness(BRIGHTNESS);
+    pixels.show();
+    delay(DELAY_MS);
+
+    pixels.setPixelColor(i, pixels.Color(0,255,0));
+    pixels.setBrightness(BRIGHTNESS);
+    pixels.show();
+    delay(DELAY_MS);
+
+    pixels.setPixelColor(i, pixels.Color(0,0,255));
+    pixels.setBrightness(BRIGHTNESS);
+    pixels.show();
+    delay(DELAY_MS);
+
+  }
+}
