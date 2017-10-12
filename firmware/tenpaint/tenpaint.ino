e5655f11c446e87267f7b1e5ed2bfdb75fc60be2
Mike Machado <mike@machadolab.com>
2017-10-12 12:49:14 -0700

    rework clear and reset mechanisms

--- firmware/tenpaint/tenpaint.ino
+++ firmware/tenpaint/tenpaint.ino
@@ -1,11 +1,13 @@
 #include <Adafruit_NeoPixel.h>
 #include <Wire.h>
 
-#define PIN 7
+#define NEO_PIN 7
+#define CLEAR_PIN 2
+#define RESET_PIN 4
 
-Adafruit_NeoPixel Neopixel = Adafruit_NeoPixel(200, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel Neopixel = Adafruit_NeoPixel(200, NEO_PIN, NEO_GRB + NEO_KHZ800);
 
-unsigned long brightness = 50;
+unsigned long brightness = 150;
 unsigned char color = 0;
 
 // Include string names of gestures/touches for testing
@@ -16,28 +18,37 @@ unsigned int max_x, max_y, max_z;
 unsigned int min_x, min_y, min_z;
 
 void setup() {
-  Serial.begin(9600);
-  while(!Serial){};
-  Serial.println("Hello world!");
-  
+
+  pinMode(CLEAR_PIN, INPUT_PULLUP);
+  pinMode(RESET_PIN, INPUT_PULLUP);
+
+  delay(1000);
+
   Neopixel.begin();
   Neopixel.setBrightness(brightness);
   Neopixel.show(); // Initialize all pixels to 'off'
- 
-  
+
+  delay(1000);
+
   Skywriter.begin(6, 5);
-  Skywriter.onTouch(touch);
-  //Skywriter.onAirwheel(airwheel);
-  Skywriter.onGesture(gesture);
+  Skywriter.onXYZ(xyz);
+  delay(1000);
+  Skywriter.begin(6, 5);
   Skywriter.onXYZ(xyz);
 }
 
 void loop() {
   Skywriter.poll();
-  /*int x;
-  for(x = 0; x < 64; x++){
-   Neopixel.setPixelColor(x, Wheel(color)); 
-  }*/
+
+  if (digitalRead(CLEAR_PIN) == LOW) {
+    Neopixel.clear();
+  }
+
+  if (digitalRead(RESET_PIN) == LOW) {
+    Skywriter.begin(6, 5);
+    Skywriter.onXYZ(xyz);
+  }
+
   Neopixel.show();
 }
 
@@ -48,64 +59,32 @@ void xyz(unsigned int x, unsigned int y, unsigned int z){
   if (x > max_x) max_x = x;
   if (y > max_y) max_y = y;
   if (z > max_z) max_z = z;
-  
+
   unsigned char pixel_x = map(x, min_x, max_x, 0, 9);
   unsigned char pixel_y = map(y, min_y, max_y, 0, 19);
   uint32_t color        = Wheel(map(z, min_z, max_z, 0, 255));
- 
-  
+
   Neopixel.setPixelColor(pixel_x + (pixel_y*10), color);
-  
-  char buf[64];
-  sprintf(buf, "%05u:%05u:%05u gest:%02u touch:%02u", x, y, z, Skywriter.last_gesture, Skywriter.last_touch);
-  Serial.println(buf);
-}
-
-void gesture(unsigned char type){
-  Serial.println("Got gesture ");
-  Serial.print(type,DEC);
-  Serial.print('\n');
-  
-  if( type == SW_FLICK_WEST_EAST ){
-     Neopixel.clear(); 
-  }
-}
-
-void touch(unsigned char type){
-  Serial.println("Got touch ");
-  Serial.print(type,DEC);
-  Serial.print('\n');
-  
-  if( type == SW_TOUCH_CENTER ){
-    Neopixel.setBrightness(map(Skywriter.x, min_x, max_x, 0, 255));
-    //color = map(Skywriter.y, min_y, max_y, 0, 255);
-  }
-  /*else if( type == SW_TOUCH_EAST ){
-     Neopixel.setBrightness(0);
-  }
-  else if( type == SW_TOUCH_WEST ){
-     Neopixel.setBrightness(255);
-  }*/
 }
 
 void airwheel(int delta){
   Serial.println("Got airwheel ");
   Serial.print(delta);
   Serial.print('\n');
-  
+
   brightness += (delta/100.0);
   Neopixel.setBrightness(brightness % 255);
 }
 
 uint32_t Wheel(byte WheelPos) {
   if(WheelPos < 85) {
-   return Neopixel.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+    return Neopixel.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
   } else if(WheelPos < 170) {
-   WheelPos -= 85;
-   return Neopixel.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+    WheelPos -= 85;
+    return Neopixel.Color(255 - WheelPos * 3, 0, WheelPos * 3);
   } else {
-   WheelPos -= 170;
-   return Neopixel.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+    WheelPos -= 170;
+    return Neopixel.Color(0, WheelPos * 3, 255 - WheelPos * 3);
   }
 }
 
